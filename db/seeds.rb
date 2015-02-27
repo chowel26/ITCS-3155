6cf78b0a38e81c02609522b36e2aa590a1de94fc
chowel26 <chowel26@uncc.edu>
2015-02-26 23:07:01 -0500

    final commit to ensure working code

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@ more_movies = [
 	{:title => 'Raiders of the Lost Ark', :rating => 'PG', :release_date => '12-Jun-1981'}
 ]
 
+Movie.send(:attr_accessible, :title, :rating, :release_date)
 more_movies.each do |movie|
 	Movie.create!(movie)
 end
